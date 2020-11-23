a1357036fc0fc944ef39b0a16f3889edef045afc
Aaditya Sharma <accolite-asharma@evernote.com>
2020-11-24 01:20:01 +0530

    Add comments on BubbleSort.java

--- src/practice/algorithms/sorting/BubbleSort.java
+++ src/practice/algorithms/sorting/BubbleSort.java
@@ -2,6 +2,23 @@ package practice.algorithms.sorting;
 
 import java.util.Arrays;
 
+/**
+ * Bubble Sort Algo
+ *
+ * It's an in-place algorithm (means we didn't have to create another
+ * array to perform the sort)
+ *
+ * O(n2) time complexity - quadratic
+ *
+ * It'll take:
+ *  -- 100 steps to sort 10 items,
+ *  -- 10,000 steps to sort 100 items,
+ *  -- 1,000,000 steps to sort 1000 items
+ *
+ * So, we can say that, this algorithm degrades quickly.
+ * Hence, should avoid using it (specially when the array size could be large)
+ */
+
 public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = {23, 32, 44, 23, 67, 23, 34, 46, 55, 83};
