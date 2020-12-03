f58497505c77cf45004002432429a6bcf5fe1bc1
Aaditya Sharma <accolite-asharma@evernote.com>
2020-12-03 16:14:23 +0530

    Updated ContainerWithMostWater.js Issue #1 https://leetcode.com/problems/container-with-most-water/

--- src/js/ContainerWithMostWater.js
+++ src/js/ContainerWithMostWater.js
@@ -1,6 +1,7 @@
 /**
  * @param {number[]} height
  * @return {number}
+ * @link {https://leetcode.com/problems/container-with-most-water/}
  */
 var maxAreaBruteforce = function(height) {
     let maxArea = 0;
@@ -16,5 +17,33 @@ var maxAreaBruteforce = function(height) {
 };
 
 console.log("Test 1 passed? : " + (maxAreaBruteforce([1,1]) === 1));
-console.log("Test 1 passed? : " + (maxAreaBruteforce([4,3,2,1,4]) === 16));
-console.log("Test 1 passed? : " + (maxAreaBruteforce([1,2,1]) === 2));
+console.log("Test 2 passed? : " + (maxAreaBruteforce([4,3,2,1,4]) === 16));
+console.log("Test 3 passed? : " + (maxAreaBruteforce([1,2,1]) === 2));
+console.log("Test 4 passed? : " + (maxAreaBruteforce([4,8,1,2,5,9]) === 32));
+console.log("Test 5 passed? : " + (maxAreaBruteforce([4,3,1,2,5,9]) === 16));
+
+/**
+ * Using two pointer technique
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxAreaOptimal = function(height) {
+    let maxArea = 0, i = 0, j = height.length - 1;
+
+    while (j > i) {
+        maxArea = Math.max(Math.min(height[i], height[j]) * (j - i), maxArea);
+        if (height[i] <= height[j]) {
+            i++;
+        } else {
+            j --;
+        }
+    }
+
+    return maxArea;
+};
+console.log("============");
+console.log("Test 1 passed? : " + (maxAreaOptimal([1,1]) === 1));
+console.log("Test 2 passed? : " + (maxAreaOptimal([4,3,2,1,4]) === 16));
+console.log("Test 3 passed? : " + (maxAreaOptimal([1,2,1]) === 2));
+console.log("Test 4 passed? : " + (maxAreaOptimal([4,8,1,2,5,9]) === 32));
+console.log("Test 5 passed? : " + (maxAreaOptimal([4,3,1,2,5,9]) === 20));
