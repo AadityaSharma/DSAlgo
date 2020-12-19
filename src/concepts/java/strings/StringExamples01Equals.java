2816ef8bcacd1f62e45eaa6f569d0d706e3ae142
Aaditya Sharma <accolite-asharma@evernote.com>
2020-12-19 23:31:48 +0530

    Stringpool demo, equality using equals() and ==

--- src/concepts/java/strings/StringExamples01Equals.java
+++ src/concepts/java/strings/StringExamples01Equals.java
@@ -5,8 +5,14 @@ public class StringExamples01Equals {
     public static void main(String[] args) {
         String x1 = "My name is Aaditya Sharma!";
         String x2 = "My name is Aaditya Sharma!";
+        String x3 = new String("My name is Aaditya Sharma!");
 
         System.out.println("Are x1 and x2 same?: " + (x1 == x2));
         System.out.println("Are x1 and x2 same?: " + (x1.equals(x2)));
+
+        System.out.println("==================");
+
+        System.out.println("Are x1 and x3 same?: " + (x1 == x3));
+        System.out.println("Are x1 and x3 same?: " + (x1.equals(x3)));
     }
 }
